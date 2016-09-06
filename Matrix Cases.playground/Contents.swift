1c275d0a058dd819fcb7c7eed6b7180f6c10f34e
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2016-09-05 20:14:20 -0700

    Initial

---
+++ Matrix Cases.playground/Contents.swift
@@ -0,0 +1,6 @@
+let a: Int = 1
+let b: Int = 2
+let c: Int = 3
+let d: Int = 4
+
+let f = ((a * b) + (c * d)) * ((a * c) + (b * d)) * ((a * d) + (b * c)) /   ((a * b) + (c * d)) * ((a * c) + (b * d)) * ((a * d) + (b * c))
